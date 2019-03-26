b38a0309064b4b36d93c66823b78cc044c1c61a8
Kevin Nguyen <kevinjnguyen2@gmail.com>
2019-03-26 18:25:37 -0500

    Added login with email and password with error handling.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:registration/home_page.dart';
+import 'package:flutter/services.dart';
 import 'package:registration/register_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   static String tag = 'login-page';
@@ -9,8 +10,24 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+  
+  String _errorMessage = '';
+
+  void onChange() {
+    setState(() {
+      _errorMessage = '';
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
+
+    emailController.addListener(onChange);
+    passwordController.addListener(onChange);
+
     final logo = Hero(
       tag: 'hero',
       child: CircleAvatar(
@@ -20,10 +37,19 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
 
+    final errorMessage = Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        '$_errorMessage',
+        style: TextStyle(fontSize: 14.0, color: Colors.red),
+        textAlign: TextAlign.center,
+      ),
+    );
+
     final email = TextFormField(
       keyboardType: TextInputType.emailAddress,
       autofocus: false,
-      initialValue: 'alucard@gmail.com',
+      controller: emailController,
       decoration: InputDecoration(
         hintText: 'Email',
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
@@ -33,8 +59,8 @@ class _LoginPageState extends State<LoginPage> {
 
     final password = TextFormField(
       autofocus: false,
-      initialValue: 'some password',
       obscureText: true,
+      controller: passwordController,
       decoration: InputDecoration(
         hintText: 'Password',
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
@@ -49,7 +75,11 @@ class _LoginPageState extends State<LoginPage> {
           borderRadius: BorderRadius.circular(24),
         ),
         onPressed: () {
-          Navigator.of(context).pushNamed(HomePage.tag);
+          signIn(emailController.text, passwordController.text).then((uid) => {
+            print(uid)
+          }).catchError((error) => {
+            processError(error)
+          });
         },
         padding: EdgeInsets.all(12),
         color: Colors.lightBlueAccent,
@@ -88,7 +118,9 @@ class _LoginPageState extends State<LoginPage> {
           padding: EdgeInsets.only(left: 24.0, right: 24.0),
           children: <Widget>[
             logo,
-            SizedBox(height: 48.0),
+            SizedBox(height: 24.0),
+            errorMessage,
+            SizedBox(height: 12.0),
             email,
             SizedBox(height: 8.0),
             password,
@@ -101,4 +133,22 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
+
+  Future<String> signIn(final String email, final String password) async {
+    FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    return user.uid;
+  }
+
+  void processError(final PlatformException error) {
+    if (error.code == "ERROR_USER_NOT_FOUND") {
+      setState(() {
+        _errorMessage = "Unable to find user. Please register.";
+      });
+    } else if (error.code == "ERROR_WRONG_PASSWORD") {
+      setState(() {
+        _errorMessage = "Incorrect password.";
+      });
+    }
+  }
+
 }
