0b289223c1a1ec4790ba2ba2cc1d40fa0d181643
Kevin Nguyen <kevinjnguyen2@gmail.com>
2019-03-29 11:44:43 -0500

    Added focus nodes.

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -21,6 +21,12 @@ class _RegisterPageState extends State<RegisterPage> {
   final passwordTextEditController = new TextEditingController();
   final confirmPasswordTextEditController = new TextEditingController();
 
+  final FocusNode _emailFocus = FocusNode();
+  final FocusNode _firstNameFocus = FocusNode();
+  final FocusNode _lastNameFocus = FocusNode();
+  final FocusNode _passwordFocus = FocusNode();
+  final FocusNode _confirmPasswordFocus = FocusNode();
+
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   @override
@@ -28,162 +34,188 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-        padding: new EdgeInsets.all(25.0),
-        child: Form(
-            key: _formKey,
-            child: ListView(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Register',
-                    style: TextStyle(fontSize: 36.0, color: Colors.black87),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value.isEmpty || !value.contains('@')) {
-                        return 'Please enter a valid email.';
-                      }
-                    },
-                    controller: emailTextEditController,
-                    keyboardType: TextInputType.emailAddress,
-                    autofocus: true,
-                    textInputAction: TextInputAction.next,
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      contentPadding:
-                          EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32.0)),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  child: TextFormField(
-                    keyboardType: TextInputType.text,
-                    autofocus: false,
-                    controller: firstNameTextEditController,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter your first name.';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'First Name',
-                      contentPadding:
-                          EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32.0)),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  child: TextFormField(
-                    keyboardType: TextInputType.text,
-                    autofocus: false,
-                    controller: lastNameTextEditController,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter your last name.';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Last Name',
-                      contentPadding:
-                          EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32.0)),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  child: TextFormField(
-                    autofocus: false,
-                    obscureText: true,
-                    controller: passwordTextEditController,
-                    validator: (value) {
-                      if (value.length < 8) {
-                        return 'Password must be longer than 8 characters.';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      contentPadding:
-                          EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32.0)),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  child: TextFormField(
-                    autofocus: false,
-                    obscureText: true,
-                    controller: confirmPasswordTextEditController,
-                    validator: (value) {
-                      if (passwordTextEditController.text.length > 8 && passwordTextEditController.text !=value) {
-                        return 'Passwords do not match.';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Confirm Password',
-                      contentPadding:
-                          EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32.0)),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top:10.0),
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                    onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        _firebaseAuth.createUserWithEmailAndPassword(email: emailTextEditController.text, password:passwordTextEditController.text).then((onValue) {
-                          Firestore.instance.collection('users').document(onValue.uid).setData({
-                            'firstName': firstNameTextEditController.text,
-                            'lastName': lastNameTextEditController.text,
-                          }).then((userInfoValue) {
-                            Navigator.of(context).pushNamed(HomePage.tag);
-                          });
-                        }).catchError((onError) {
-                          print(onError);
-                        });
-                      }
-                    },
-                    padding: EdgeInsets.all(12),
-                    color: Colors.lightGreen,
-                    child: Text('Sign Up'.toUpperCase(),
-                        style: TextStyle(color: Colors.white)),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.zero,
-                  child: FlatButton(
+          padding: new EdgeInsets.all(25.0),
+          child: Form(
+              key: _formKey,
+              child: ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
                     child: Text(
-                      'Cancel',
-                      style: TextStyle(color: Colors.black54),
+                      'Register',
+                      style: TextStyle(fontSize: 36.0, color: Colors.black87),
+                      textAlign: TextAlign.center,
                     ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  )
-                )
-              ],
-            )
-          )
-        ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty || !value.contains('@')) {
+                          return 'Please enter a valid email.';
+                        }
+                      },
+                      controller: emailTextEditController,
+                      keyboardType: TextInputType.emailAddress,
+                      autofocus: true,
+                      textInputAction: TextInputAction.next,
+                      focusNode: _emailFocus,
+                      onFieldSubmitted: (term) {
+                        FocusScope.of(context).requestFocus(_firstNameFocus);
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0)),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter your first name.';
+                        }
+                      },
+                      controller: firstNameTextEditController,
+                      keyboardType: TextInputType.text,
+                      autofocus: false,
+                      textInputAction: TextInputAction.next,
+                      focusNode: _firstNameFocus,
+                      onFieldSubmitted: (term) {
+                        FocusScope.of(context).requestFocus(_lastNameFocus);
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'First Name',
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0)),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter your last name.';
+                        }
+                      },
+                      controller: lastNameTextEditController,
+                      keyboardType: TextInputType.text,
+                      autofocus: false,
+                      textInputAction: TextInputAction.next,
+                      focusNode: _lastNameFocus,
+                      onFieldSubmitted: (term) {
+                        FocusScope.of(context).requestFocus(_passwordFocus);
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Last Name',
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0)),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.length < 8) {
+                          return 'Password must be longer than 8 characters.';
+                        }
+                      },
+                      autofocus: false,
+                      obscureText: true,
+                      controller: passwordTextEditController,
+                      textInputAction: TextInputAction.next,
+                      focusNode: _passwordFocus,
+                      onFieldSubmitted: (term) {
+                        FocusScope.of(context).requestFocus(_confirmPasswordFocus);
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0)),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: TextFormField(
+                      autofocus: false,
+                      obscureText: true,
+                      controller: confirmPasswordTextEditController,
+                      focusNode: _confirmPasswordFocus,
+                      textInputAction: TextInputAction.done,
+                      validator: (value) {
+                        if (passwordTextEditController.text.length > 8 &&
+                            passwordTextEditController.text != value) {
+                          return 'Passwords do not match.';
+                        }
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Confirm Password',
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0)),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          _firebaseAuth
+                              .createUserWithEmailAndPassword(
+                                  email: emailTextEditController.text,
+                                  password: passwordTextEditController.text)
+                              .then((onValue) {
+                            Firestore.instance
+                                .collection('users')
+                                .document(onValue.uid)
+                                .setData({
+                              'firstName': firstNameTextEditController.text,
+                              'lastName': lastNameTextEditController.text,
+                            }).then((userInfoValue) {
+                              Navigator.of(context).pushNamed(HomePage.tag);
+                            });
+                          }).catchError((onError) {
+                            print(onError);
+                          });
+                        }
+                      },
+                      padding: EdgeInsets.all(12),
+                      color: Colors.lightGreen,
+                      child: Text('Sign Up'.toUpperCase(),
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.zero,
+                      child: FlatButton(
+                        child: Text(
+                          'Cancel',
+                          style: TextStyle(color: Colors.black54),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ))
+                ],
+              ))),
     );
   }
 }
